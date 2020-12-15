fbdf7b81e3152133fefd4de2555e73db5e782e0a
Matthew Harris <matthewharrisak@gmail.com>
2020-12-15 14:21:30 -0600

    added material ui form component for the add-new-particiapant button

--- src/components/PartDisplay/PartDisplay.js
+++ src/components/PartDisplay/PartDisplay.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-
+import AddPartButt from '../AddPartButt/AddPartButt';
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -21,7 +21,7 @@ class PartDisplay extends Component {
       <div>
         <h2>this is where we'll display the participants table for admins</h2>
         {JSON.stringify(this.props.store.participants)}
-        
+        <AddPartButt/>
       </div>
     );
   }
